b9227bca2d4890549f5facff77617f67aa9662f1
impradp <impradp@gmail.com>
2024-06-22 23:03:40 +0545

    ORR: Initial Commit

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace OwlReadingRoom
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace OwlReadingRoom
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
