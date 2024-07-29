56b584a7e91694cd36555d5911bfaa2ca9c5bb46
mohendra.amatya <mohendra.amatya@esewa.com.np>
2024-07-29 23:40:13 +0545

    Added a connection service Added a repository interface

---
+++ Services/Database/DatabaseConnectionService.cs
@@ -0,0 +1,25 @@
+﻿using OwlReadingRoom.Model;
+using OwlReadingRoom.Services.Constants;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OwlReadingRoom.Services.Database
+{
+    public class DatabaseConnectionService : IDatabaseConnectionService
+    {
+        private SQLiteAsyncConnection connection;
+        public SQLiteAsyncConnection Connection { get => connection; }
+
+        public async Task Init<T>() where T : BaseModel, new()
+        {
+            if (connection is not null)
+                return;
+            connection = new SQLiteAsyncConnection(DataBaseConstants.DatabasePath, DataBaseConstants.Flags);
+            await connection.CreateTableAsync<T>();
+        }
+    }
+}
---
+++ Services/Repository/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using OwlReadingRoom.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OwlReadingRoom.Services.Repository
+{
+    public interface IRepository<T> where T : BaseModel, new()
+    {
+        Task<int> DeleteItemAsync(T item);
+        Task<List<T>> Get<TValue>(Expression<Func<T, bool>> predicate = null, Expression<Func<T, TValue>> orderBy = null);
+        Task<T> GetItemAsync(int id);
+        Task<List<T>> GetItemsAsync();
+        Task<T> GetItemsAsync(Expression<Func<T, bool>> predicate);
+        Task<int> SaveItemAsync(T item);
+    }
+}
