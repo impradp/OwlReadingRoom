764e4d1e43cfa04067db4cf72a696f6b1efb036a
JVMA1994 <amatya.mohendra@gmail.com>
2024-10-02 12:14:50 +0545

    Notify through email the subscription of a new package Add a new email template for customer pacakge subscription

--- Services/CustomerDetailsService.cs
+++ Services/CustomerDetailsService.cs
@@ -1,4 +1,5 @@
 ﻿
+using OwlReadingRoom.DTOs;
 using OwlReadingRoom.Models;
 using OwlReadingRoom.Services.Transactions;
 using OwlReadingRoom.ViewModels;
@@ -41,6 +42,50 @@ public class CustomerDetailsService : ICustomerDetailsService
             Documents = documentInformation,
             Status = minimumInformation.Status.ToString()
         };
+    }
+
+    public MinimumCustomerDetail GetCustomerMinimumDetail(int customerId)
+    {  
+        PersonalDetail personalDetail = _customerService.GetPersonalDetails(customerId);
+        return new MinimumCustomerDetail
+        {
+            FullName = FormatFullNameWithHonorific(personalDetail.Gender, personalDetail.FullName)
+        };
+    }
+
+    /// <summary>
+    /// Formats the full name of the customer by adding an appropriate honorific based on gender.
+    /// </summary>
+    /// <param name="gender">The gender of the customer, which may be null or empty.</param>
+    /// <param name="fullName">The full name of the customer.</param>
+    /// <returns>A string representing the full name prefixed with the appropriate honorific.</returns>
+    private string FormatFullNameWithHonorific(string gender, string fullName)
+    {
+        string honorific = GetHonorific(gender);
+        return $"{honorific}{fullName}";
+    }
+
+    /// <summary>
+    /// Retrieves the appropriate honorific based on the customer's gender.
+    /// </summary>
+    /// <param name="gender">The gender of the customer. Can be null or empty.</param>
+    /// <returns>A string representing the honorific (e.g., "Mr.", "Ms.", "Mx.") or an empty string if no valid gender is provided.</returns>
+    private string GetHonorific(string gender)
+    {
+        // Handle null or empty gender directly
+        if (string.IsNullOrEmpty(gender))
+        {
+            return string.Empty; // Return empty string if gender is null or empty
+        }
+
+        var honorifics = new Dictionary<string, string>
+    {
+        { "Male", "Mr. " },
+        { "Female", "Ms. " },
+        { "Other", "Mx. " }
+    };
+
+        return honorifics.TryGetValue(gender, out var honorific) ? honorific : string.Empty;
     }
 
     public void RenewCustomerBooking(CustomerDetailViewModel customerDetail)
